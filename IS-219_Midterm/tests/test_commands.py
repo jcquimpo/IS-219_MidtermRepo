8bf52c5997d32ebc43803140f5fea380b0948e28
JanPotat0 <jancquimpo@gmail.com>
2024-03-25 16:42:24 -0400

    follow-up commit - 3/25

--- IS-219_Midterm/tests/test_commands.py
+++ IS-219_Midterm/tests/test_commands.py
@@ -2,33 +2,11 @@
 
 import pytest
 from app import App
-from dotenv import load_dotenv
+# from app.plugins.greet import GreetCommand
 from app.plugins.add import AddCommand
 from app.plugins.subtract import SubtractCommand
 from app.plugins.multiply import MultiplyCommand
 from app.plugins.divide import DivideCommand
-
-def test_app_greet_command(capfd, monkeypatch):
-    """Test that the REPL correctly handles the 'greet' command."""
-    inputs = iter(['greet', 'exit'])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-
-    app = App()
-    with pytest.raises(SystemExit) as e:
-        app.start()
-
-    assert str(e.value) == "0", "The app did not exit as expected"
-
-def test_app_menu_command(capfd, monkeypatch):
-    """Test that the REPL correctly handles the 'greet' command."""
-    inputs = iter(['menu', 'exit'])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-
-    app = App()
-    with pytest.raises(SystemExit) as e:
-        app.start()
-
-    assert str(e.value) == "0", "The app did not exit as expected"
 
 def test_add_command(capfd, monkeypatch):
     """Test the execution of the AddCommand."""
