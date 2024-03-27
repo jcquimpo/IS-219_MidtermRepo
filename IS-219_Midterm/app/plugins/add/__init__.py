e7cc316e575b13e1e3b66c06cfbeb79574933f5b
JanPotat0 <jancquimpo@gmail.com>
2024-03-27 15:41:40 -0400

    added history printing function for csv command and logging commands and numbers used

--- IS-219_Midterm/app/plugins/add/__init__.py
+++ IS-219_Midterm/app/plugins/add/__init__.py
@@ -1,5 +1,6 @@
 from app.commands import Command
 import logging
+import csv
 
 class AddCommand(Command):
     def execute(self):
@@ -8,7 +9,12 @@ class AddCommand(Command):
             num1 = float(num1)
             num2 = float(num2)
             result = num1 + num2
-            print("The sum of {} and {} is: {}".format(num1, num2, result))
+            print(f"The sum of {num1} and {num2} is: {result}")
             logging.info(f'The numbers added are num1: {num1} & num2: {num2}; the sum is {result}')
+            
+            with open("./data/operations_history.csv", "a", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['add', num1, num2, result])
+            
         except ValueError:
             print("Please enter valid numbers.")
--- IS-219_Midterm/app/plugins/divide/__init__.py
+++ IS-219_Midterm/app/plugins/divide/__init__.py
@@ -1,5 +1,6 @@
 from app.commands import Command
 import logging
+import csv
 
 class DivideCommand(Command):
     def execute(self):
@@ -7,8 +8,19 @@ class DivideCommand(Command):
         try:
             num1 = float(num1)
             num2 = float(num2)
+            
+            if num2 == 0:
+                print("Cannot divide by zero. Please enter a non-zero divisor.")
+                logging.error("Attempted to divide by zero.")
+                return
+            
             result = num1 / num2
             print("The quotient of {} and {} is: {}".format(num1, num2, result))
             logging.info(f'The numbers divided are num1: {num1} & num2: {num2}; the quotient is {result}')
+            
+            with open("./data/operations_history.csv", "a", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['divide', num1, num2, result])
+            
         except ValueError:
             print("Please enter valid numbers.")
